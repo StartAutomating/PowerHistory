572b34db68fc944ca2ca00d6c9604b13c985770b
James Brundage <james.brundage@start-automating.com>
2020-03-02 22:15:10 -0800

    Initial Commit of PowerHistory module

---
+++ PowerHistory.psd1
@@ -0,0 +1,10 @@
+﻿@{
+    ModuleVersion = 0.1
+    RootModule = 'PowerHistory.psm1'
+    Guid = '8ebdb46b-fa97-4ec5-82d1-2501288ae013'
+    FormatsToProcess = 'PowerHistory.format.ps1xml'
+    Description = 'PowerShell History Made More Powerful'
+    AliasesToExport = 'aph','gph','iph','tph'
+    Copyright = '2020 Start-Automating'
+    Author = 'James Brundage'
+}
---
+++ PowerHistory.psm1
@@ -0,0 +1,16 @@
+﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', "", Justification = 'Functionality Most Be Global')]
+param()
+foreach ($file in Get-ChildItem -Path $PSScriptRoot -Filter '*-*.ps1') {
+    . $file.FullName
+}
+
+Set-Alias aph Add-PowerHistory
+Set-Alias gph Get-PowerHistory
+Set-Alias tph Trace-PowerHistory
+Set-Alias iph Invoke-PowerHistory
+
+if (-not $Global:PowerHistory) {
+    $Global:PowerHistory = [Collections.Generic.Dictionary[string, PSObject]]::new([StringComparer]::OrdinalIgnoreCase)
+}
+
+Export-ModuleMember -Function *-* -Alias *
